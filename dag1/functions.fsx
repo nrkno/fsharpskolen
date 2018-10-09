ea1f1f77d37d898d9c95f20476473ae2c2a6d6f4
Heidi Mork <heidimork@yahoo.no>
2018-10-10 00:39:43 +0200

    option

--- dag1/functions.fsx
+++ dag1/functions.fsx
@@ -1,8 +1,21 @@
 
-let areaOfSquare x = x * x
+let square x = x * x
 
-let doubleSquareSide x = 2 * x
+let double x = 2 * x
 
-let areaOfDoubleSquare = doubleSquareSide >> areaOfSquare
+let areaOfDouble = double >> square
 
-let areaOfDoubleSquare2  x = x |> doubleSquareSide |> areaOfSquare
+let areaOfDoubleSquare2  x = x |> double |> square
+
+let hypotenuseSquared a b = square a + square b
+
+//let euclideanNumbers a b = 
+
+
+let isPythagoreanTriple a b c = 
+    if a > b && a > c then square a = square b + square c
+    else if b > a && b > c then square b = square a + square c
+    else square c = square a + square b
+
+
+
--- dag1/recursion-solution.fsx
+++
@@ -1,8 +0,0 @@
-
-let isPrime x = 
-    let rec helper x curr = 
-        if curr = x then true 
-        else if x % curr = 0 then false 
-        else helper x (curr + 1)
-    helper x 2
-        
