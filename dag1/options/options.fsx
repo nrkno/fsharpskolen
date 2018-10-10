f78c312a801497c13c8c44a006f721469def980e
Heidi Mork <Heidi.Mork@nrk.no>
2018-10-10 12:12:36 +0200

    options!

---
+++ dag1/options/options.fsx
@@ -0,0 +1,46 @@
+// Tenk at du trenger å konvertere int til et ikke-negativt tall
+// En måte å gjøre det på (særlig før vi har lært så mye om andre typer) er å lage en funksjon som returnerer en option, 
+// med None hvis tallet du sender inn er negativt 
+let nonNegative x = None
+
+// Hvordan skal vi addere sammen to nonNegative tall?
+// Lag en funksjon som bruker pattern matching
+let add x y = None
+
+// Option har også en egen høyereordens funksjon, Option.map2, som kan gjøre matchingen for oss
+// Lag add2 med denne funksjonen istedet
+let add2 x y = None
+
+// Test at funksjonene virker som forventet
+printfn "add 2 og 3: %A" (add (nonNegative 2) (nonNegative 3))
+printfn "add None og 4: %A" (add (nonNegative -2) (nonNegative 4))
+printfn "add 2 og 3: %A" (add2 (nonNegative 2) (nonNegative 3))
+printfn "add None og 4: %A" (add2 (nonNegative -2) (nonNegative 4))
+
+// La oss leke litt med strenger også
+// Lag ferdig funksjonen stringLength som tar inn en string option og returnerer en int option   
+let stringLength (str : string option) = None
+    
+// Det går også an å hente ut intverdien direkte med feks Option.fold
+let stringLengthFold  (str : string option) = 0
+
+printfn "string length option: %A" (stringLength (Some "Hei"))
+printfn "String length int: %d" (stringLengthFold (Some "Hei"))
+
+
+// Hva om vi har to string options og vil sette de sammen til en streng?
+// Hva er det fornuftig å returnere om den ene optionen er None?
+let concat (str1 : string option) (str2 : string option) = None      
+    
+// En fin ting med string.Length er at den er en ikke-negativ int, så la oss kombinere funksjoner vi har laget til 
+// å finne lengden av konkatering av to strenger, og her kan man gå to veier, 
+// enten finne lengden av hver av strengene og så legge sammen
+// eller konkatenere strengene og så finne lengen
+// de to veiene bør gi samme resultat
+// Funksjonene kan implementeres med bare å bruke stringLenght, add og concat    
+let addTwoLengths  (str1 : string option) (str2 : string option) = None
+
+let lenghtOfConcat (str1 : string option) (str2 : string option) = None
+
+    
+    
