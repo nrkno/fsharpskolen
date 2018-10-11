ea9641f6fabc0a4a87d98379bee5ed563a18a42a
Einar W. Høst <einarwh@gmail.com>
2018-10-11 18:40:49 +0200

    Simple String.module.

---
+++ dag2/modules/String-solution.fsx
@@ -0,0 +1,15 @@
+module String = 
+
+    let substring start stop (s : string) = 
+        s.Substring(start, stop)
+  
+    let crop maxlength s = 
+        if String.length s > maxlength then substring 0 maxlength s else s
+    
+    let reverse (s : string) = 
+        Seq.fold (fun result c -> string(c) + result) "" s
+   
+"hello world" 
+|> String.substring 1 9 
+|> String.crop 4 
+|> String.reverse
