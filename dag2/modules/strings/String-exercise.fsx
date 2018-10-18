3e75d53196c94417bd18df655991139c83e464dc
Einar W. Høst <einarwh@gmail.com>
2018-10-18 12:03:27 +0200

    Hjelpefunksjoner for tekststrenger.

--- dag2/modules/strings/String-exercise.fsx
+++ dag2/modules/strings/String-exercise.fsx
@@ -9,14 +9,24 @@ module String =
     let substring (start : int) (stop : int) (s : string) = s
   
     // Funksjonen crop skal sørge for at en tekststreng ikke er lengre enn en gitt makslengde.
+    // F.eks: crop 4 "kokkelue" -> "kokk"
     let crop (maxlength : int) (s : string) = s
+
+    // Funksjonen undup skal fjerne etterfølgende duplikater av tegn.
+    // F.eks: undup "lappeteppe" -> "lapetepe"
+    let undup (s : string) = s
     
     // Funksjonen reverse skal gi en tekststreng som inneholder tegnene i input-strengen i motsatt rekkefølge.
+    // (Merk at reverse >> reverse bør gi den opprinnelige tekststrengen!)
+    // F.eks: reverse "langøre" -> "erøgnal"
     let reverse (s : string) = s
 
 // Dette burde gi... hva da?   
 "hello world" 
 |> String.substring 1 9     // ello worl
 |> String.crop 4            // ello
-|> String.reverse           // olle
+|> String.undup             // elo
+|> String.reverse           // ole
 |> printfn "%s"
+
+// Hvordan lager du en okse av en hestesko? Finner du mer enn en måte?
--- dag2/modules/strings/String-solution.fsx
+++ dag2/modules/strings/String-solution.fsx
@@ -1,16 +1,43 @@
 module String = 
 
-    let substring start stop (s : string) = 
-        s.Substring(start, stop)
+    let substring start length (s : string) = 
+        s.Substring(start, length)
   
     let crop maxlength s = 
         if String.length s > maxlength then substring 0 maxlength s else s
-    
+
+    let undup : string -> string = 
+        let rec unduplist lst = 
+            match lst with 
+            | [] -> []
+            | [c] -> [c]
+            | x :: y :: t -> 
+                if x = y then unduplist (y :: t) 
+                else x :: unduplist (y :: t)
+        Seq.toList >> unduplist >> List.toArray >> System.String
+         
     let reverse (s : string) = 
         Seq.fold (fun result c -> string(c) + result) "" s
-   
+
+"###f###" |> String.substring 3 2 |> printfn "%s"
+"kokkelue" |> String.crop 4 |> printfn "%s"
+"lappeteppe" |> String.undup |> printfn "%s"
+"langøre" |> String.reverse |> printfn "%s"
+"langøre" |> (String.reverse >> String.reverse) |> printfn "%s"
+
 "hello world" 
 |> String.substring 1 9     // ello worl
 |> String.crop 4            // ello
-|> String.reverse           // olle
+|> String.undup             // elo
+|> String.reverse           // ole
+|> printfn "%s"
+
+"hestesko"
+|> String.reverse
+|> String.crop 4
+|> printfn "%s"
+
+"hestesko"
+|> String.substring 4 4 
+|> String.reverse
 |> printfn "%s"
