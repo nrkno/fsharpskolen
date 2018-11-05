c26ac2bd648048af5dd3939719a40728f3a662b4
Heidi Mork <Heidi.Mork@nrk.no>
2018-11-05 18:08:21 +0100

    progid-oppgave

---
+++ dag3/progid/Program.fs
@@ -0,0 +1,4 @@
+// ProgId er en id for et program, og består av 12 tegn, de fire første tegnene er bokstaver, de 8 siste er tall
+// Definer typen ProgId, og bruk "modul-strategien" til å gjøre det umulig å lage en ugyldig progid
+
+type ProgId = string 
