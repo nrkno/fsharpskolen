7b9b57e71849fea87ea4fe2e1053c217f79cbd5b
Einar W. Høst <einarwh@gmail.com>
2018-11-06 09:41:37 +0100

    Update dag3/progid/Program.fs

--- dag3/progid/Program.fs
+++ dag3/progid/Program.fs
@@ -1,4 +1,6 @@
 // ProgId er en id for et program, og består av 12 tegn, de fire første tegnene er bokstaver, de 8 siste er tall
 // Definer typen ProgId, og bruk "modul-strategien" til å gjøre det umulig å lage en ugyldig progid
+// Gyldig progId: MSPO40820417, MSUI13008317
+// Ugyldig progId: MSUI1300831712, MSUI130O8317, M5UI13008317
 
 type ProgId = string 
