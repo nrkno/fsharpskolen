e8ab57bfca9feeb135dd39528e603e2ae7d246a2
Einar W. Høst <einarwh@gmail.com>
2018-11-20 12:14:22 +0100

    added some files

---
+++ dag4/program-api/src/ProgramApi/Data.fs
@@ -0,0 +1,38 @@
+﻿module ProgramApi.Data
+
+open NodaTime
+open NodaTime.Text
+
+type ContributorDto = {
+    Role : string 
+    Name : string 
+}
+
+type LegalAgeDto = {
+    Code : string 
+    Description : string
+    Label : string
+}
+
+type TransmissionDto = {
+    Channel : string
+    EpgDate : string
+    StartTime : string
+    EndTime : string
+}
+
+type PlayableDto = {
+    Manifest : string 
+    Subtitles : string 
+}
+
+type IndexPointDto = {
+    StartPoint : string 
+    Title : string 
+}
+
+type PlaybackElementDto = {
+    Playable : PlayableDto  
+    Duration : string 
+    IndexPoints : IndexPointDto list 
+}
