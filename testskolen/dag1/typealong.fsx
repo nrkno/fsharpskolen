27a46a02478489306d9624e4057b713d19425ae1
Heidi Mork <Heidi.Mork@nrk.no>
2019-03-06 10:18:41 +0100

    typealong

--- testskolen/dag1/typealong.fsx
+++ testskolen/dag1/typealong.fsx
@@ -10,5 +10,93 @@ true
 
 5 / 2
 
+0.5
+
 let x = 3 + 4
+
+let y = 2
+
+x + y
+
+// Functions
+
+let add x y = x + y
+
+let concat x y : string = x + y
+
+
+// Lists 
+
+[] // parametric polymorphism
+
+[1]
+
+1 :: []
+
+[1; 2; 3]
+
+1 :: 2 :: 3 :: []
+
+[1; 2] @ [3; 4]
+
+[1 .. 10]
+
+[1 .. 3 .. 10]
+
+[ for i in [1; 2; 3] do
+    for j in [4; 5; 6] do
+      yield i + j ]
+
+[1; 2; 3; 4]
+|> List.map (fun n -> n * n)
+|> List.filter (fun n -> n > 3)
+
+
+// Optional - no null!
+
+Some "world"
+None
+
+let greet name =
+    match name with
+    | Some str -> printfn "Hello %s!" str
+    | None -> printfn "Hello!"
+
+greet (Some "world")
+greet None
+
+// Discriminated union
+type Direction = North | East | South | West
+
+
+type Card = Visa | Master
+Visa
+Master
+
+type PhoneNumber = PhoneNumber of int
+PhoneNumber 98765432
+
+type PaymentMethod =
+    | Cash
+    | Vipps of PhoneNumber
+    | Card of Card
+
+
+Cash
+Vipps (PhoneNumber 98765432)
+Card Visa
+
+// Records
+
+type PaymentAmount = PaymentAmount of decimal
+type Currency = NOK | EUR
+type Payment = {
+    Amount: PaymentAmount
+    Currency: Currency
+    Method: PaymentMethod
+}
+
+
+
+
 
