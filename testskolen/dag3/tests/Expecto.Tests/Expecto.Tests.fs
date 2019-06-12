4c2db9749891ff33d0c078534794898ca433df72
Heidi Mork <heidimork@yahoo.no>
2019-06-12 19:13:47 +0200

    more expecto

--- testskolen/dag3/tests/Expecto.Tests/Expecto.Tests.fs
+++ testskolen/dag3/tests/Expecto.Tests/Expecto.Tests.fs
@@ -1,6 +1,18 @@
 module Expecto.Tests
 
 open Expecto
+open System
+
+let random = Random()
+
+let invoke (setup: unit -> 'a) (tearDown: 'a -> unit ) (test : 'a -> Test) = 
+    let id = setup ()
+    try
+        printfn "Running test for id %d" id
+        test id
+    finally
+        tearDown id
+
 
 let tests = testList "A list of tests" [
   test "A simple test" {
@@ -11,11 +23,25 @@ let tests = testList "A list of tests" [
     Expect.equal (1 + 1) 2 "One plus one is two"
   }]
 
-let otherTests = testList "Another list of tests" [
-  test "" {
-      let text = "Hello"
-      Expect.equal (String.length text) 5 "String length should be as expected"
-  }
+let otherTests =
+  let setup () =
+    let id = random.Next(0, 10000)
+    printfn "Setup id %d before test" id
+    id
+
+  let tearDown id = 
+    printfn "Tidy up things with id %d after test" id
+  
+  let invoke = invoke setup tearDown
+
+  testList "Another list of tests" [
+  invoke (fun x -> test "x + x = 2 * x" {
+      Expect.equal (x + x) (2 * x) "x + x = 2 * x"
+  })
+  invoke (fun x -> test "sqrt x * x = x" {
+    let sqrt = x |> float |> (fun x -> Math.Sqrt (x * x)) |> int
+    Expect.equal sqrt x "sqrt x * x = x"
+  })
 ]
 
 [<EntryPoint>]
