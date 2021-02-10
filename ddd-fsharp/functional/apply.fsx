ac7fa15f64073b878759d033c339a7ae171a0a6f
bjartwolf <bjartnes@gmail.com>
2021-02-10 11:13:40 +0100

    add example example

--- ddd-fsharp/functional/apply.fsx
+++ ddd-fsharp/functional/apply.fsx
@@ -1,7 +1,13 @@
+
+
 // fra https://fsharpforfunandprofit.com/posts/elevated-world/#the-return-function 
 let add y x = x + y
 let add10 x = add 10 x
 let add20 x = add 20 x
+
+
+// it helps thinking of these things as partial applications, small steps...
+let foo = List.map add20
 
 module Option =
 
@@ -33,4 +39,10 @@ module List =
     let unpackedFoo = apply foo// Then we can apply stuff in some-land
     unpackedFoo bar 
 
+module Async = 
+//Borrowed from https://github.com/fsprojects/FSharpPlus/blob/master/src/FSharpPlus/Extensions/Async.fs#L47
+    let apply f x = async.Bind (f, fun x1 -> async.Bind (x, fun x2 -> async {return x1 x2}))
 
+    let foo = async { return add10 }
+
+    let unpackedFoo = apply foo 
