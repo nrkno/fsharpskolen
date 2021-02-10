22163c2c6ccaf12ca9ac6f05b4b59782f19c6439
bjartwolf <bjartnes@gmail.com>
2021-02-10 10:57:32 +0100

    add example example

--- ddd-fsharp/functional/apply.fsx
+++ ddd-fsharp/functional/apply.fsx
@@ -13,7 +13,8 @@ module Option =
 
     let foo = Some add10 // Some is return here and lifts/returns the function to Some-land 
     let bar = Some 20  // And we can lift/return 20 to Some-land too
-    apply foo bar // Then we can apply stuff in some-land
+    let unpackedFoo = apply foo
+    unpackedFoo bar // Then we can apply stuff in some-land
 
 module List =
 
@@ -23,8 +24,13 @@ module List =
         [ for f in fList do
           for x in xList do
               yield f x ]
+    
+    let return' x = [x]
 
     let foo = [add10; add20] // We can lift/return both these, but not sure this is really lift when we do it with both?
     let bar = [ 10; 20] // and this too... Not really sure if it is lift/return in the same way
-    apply foo bar
+    
+    let unpackedFoo = apply foo
+    unpackedFoo bar // Then we can apply stuff in some-land
+
 
