a245a03be60c7cd1476bc8cdc2d53f4fb01f5da8
bjartwolf <bjartnes@gmail.com>
2021-02-11 07:49:05 +0100

    cleaning up and making maps

--- ddd-fsharp/functional/apply.fsx
+++ ddd-fsharp/functional/apply.fsx
@@ -1,30 +1,52 @@
-
-
 // fra https://fsharpforfunandprofit.com/posts/elevated-world/#the-return-function 
+
+// This little page will introduce apply and pure and combine them to map
+// For Options, Lists and Async
+
+// Just some normal functions to start with
 let add y x = x + y
 let add10 x = add 10 x
 let add20 x = add 20 x
 
-
-// it helps thinking of these things as partial applications, small steps...
-let foo = List.map add20
+// Sums the char code of the letters in a string
 let sumOfString (s:string) = Array.sumBy int (System.Text.Encoding.ASCII.GetBytes(s))
 
-module Option =
+// It helps thinking of these things as partial applications, small steps...
+// For me it clicked a bit thinking of partial application of map instead of "using the map on a list" 
+let foo = List.map add20
 
+module Option =
+    // a -> E<a>, lifts a single value to the elevated world
+    // In this case
+    // a -> Some<a>, lifts a single value to the option world
+    let pure' x = Some x 
+    let notpure x = None // this will not hold the applicative laws, but it has the same signature 
+
+
+    // Because functions also are values, they too can be lifted with pure
+    // (a->b) -> E<(a->b)>
+    // Pure lifts the function int -> int to a (int -> int) option
+    // Bar is E<(int->int)>
+    let bar = (pure' add10) 
+    // Foo is E<(int->string)>
+    let foo = (pure' sumOfString ) 
+  
+    // Apply: E<(a->b)> -> E<a> -> E<b>
+    // In the option case here
+    // (a->b) option -> a option -> b option 
     // The apply function for Options
     let apply fOpt xOpt = 
         match fOpt,xOpt with
         | Some f, Some x -> Some (f x)
         | _ -> None
 
-    let pure' x = Some x 
-    let pure'' x = None // this will not hold the applicative laws 
+    apply (pure' id) (Some 5) = Some 5  // Holds the law
+    apply (notpure id) (Some 6) = Some 5 // Fails the law
 
-    apply (pure' id) (Some 5) = Some 5  // Holds
-    apply (pure'' id) (Some 6) = Some 5 // Fails
-
-    let add10Applied = apply (pure' add10) // E<int> -> E<int>
+    // The applied function takes the
+    // E<(int->int)> -> E<int> -> E<int>, which in this case is
+    // (int->int) option to a int option -> int option
+    let add10Applied = apply (pure' add10) 
     add10Applied (pure' 20) 
 
     let appliedStringLength = apply (pure' String.length) 
@@ -37,46 +59,62 @@ module Option =
     let appliedSumOfString = apply (pure' sumOfString) // E<string> -> E<int>
     appliedSumOfString (pure' "Hei")
 
+    let map' fn = apply (pure' fn)
 
 // We could probably do this for a lot of collections like array and set and I am not sure how many...
-// They would also most likely be very similar
+// They would also most likely be very similar, so we pick lists
 module List =
+    // a -> E<a>, lifts a single value to the elevated world
+    // In this case
+    // a -> List<a>, lifts a single value to the list world
+    let pure' x = [x]
+    let notpure x = [x; x] 
+
     // The apply function for lists
     // [f;g] apply [x;y] becomes [f x; f y; g x; g y]
+    // E<(a->b)> -> E<a> -> E<b>
     let apply (fList: ('a->'b) list) (xList: 'a list)  = 
         [ for f in fList do
           for x in xList do
               yield f x ]
     
-    let pure' x = [x]
-    let pure'' x = [x; x] 
-
     apply (pure' id) [5] = [5]
-    apply (pure'' id) [5] = [5]
-
-    let map' fn = apply (pure' fn)
+    apply (notpure id) [5] = [5]
 
     apply [add10; add20] [3; 7]
 
+    // Lifts the (int->int) to List world with pure to be a List<(int->int)>
+    // Then applies it and it turns into a List<int> -> List<int>
     let add10Applied = apply (pure' add10) // E<int> -> E<int>
+
     add10Applied (pure' 20) 
+    add10Applied [1; 2; 3] // But this is map! 
+
+    // So let us make map with apply and pure
+    let map' fn = apply (pure' fn)
+
+    (map' add10) [2;3;4]
+
 
 module Async = 
-//Borrowed from https://github.com/fsprojects/FSharpPlus/blob/master/src/FSharpPlus/Extensions/Async.fs#L47
+    // a -> E<a>, lifts a single value to the elevated world
+    // In this case
+    // a -> Async<a>, lifts a single value to the async world
+    let pure' (value : 'a) : Async<'a> = async.Return value
+    let notpure (value : 'a) : Async<'a> = async { do! Async.Sleep(10)
+                                                   return value}
+
+    //Apply is Borrowed from https://github.com/fsprojects/FSharpPlus/blob/master/src/FSharpPlus/Extensions/Async.fs#L47
     let apply f x = async.Bind (f, fun x1 -> async.Bind (x, fun x2 -> async {return x1 x2}))
 
-    let pure' (value : 'a) : Async<'a> = async.Return value
-    let pure'' (value : 'a) : Async<'a> = async { do! Async.Sleep(10) 
-                                                  return value } 
-
-    // This is most likely cheating, because they should be compared before they are 
-    // run and they are most certainly not equal at some point in time when one has completed and the other has not
+    // Set up the timeout is 1ms to help explain why they are not similar, even it they at some point in time will be
     let runSyncWithTimeout task = Async.RunSynchronously(task, 1)
     (apply (pure' id) (async.Return 5) |> runSyncWithTimeout) = ((async.Return 5) |> runSyncWithTimeout) 
-    apply (pure'' id) (async.Return 5) |> runSyncWithTimeout = ((async.Return 5) |> runSyncWithTimeout) 
+    apply (notpure id) (async.Return 5) |> runSyncWithTimeout = ((async.Return 5) |> runSyncWithTimeout) 
 
     // alternative apply fra einar
     // let bind fn value = async.Bind(value, fn)
-    //    let apply wrappedFn wrappedValue = wrappedFn |> bind (fun fn -> wrappedValue |> bind (fun value -> pure (fn value)))
+    // let apply wrappedFn wrappedValue = wrappedFn |> bind (fun fn -> wrappedValue |> bind (fun value -> pure (fn value)))
+
     let add10Applied = apply (pure' add10) 
     add10Applied (pure' 20) |> Async.RunSynchronously
