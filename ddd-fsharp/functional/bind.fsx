737a96b0fd6a147b2dcfc9ecf92acd2e8258e596
bjartwolf <bjartnes@gmail.com>
2021-02-11 10:53:21 +0100

    ideas

--- ddd-fsharp/functional/bind.fsx
+++ ddd-fsharp/functional/bind.fsx
@@ -1,3 +1,17 @@
+// In the previous apply section we looked at unit, apply and map to lift functions
+// from normal world to elevated worlds
+
+// But what if the functions are crossing the world, that is we have string -> Async<int> ?
+// Or more general, a -> E<b>
+// With map we could take a (a->b) to a E<a> -> E<b>
+// In a similar way we can - using bind - take an a -> E<b> and make a E<a> -> E<b>
+
+// Takes a string and returns a list of its charcodes 
+// This is a function from a -> E<b> with a being a string, E the list world and b int
+let sumOfString (s:string) = Array.map int (System.Text.Encoding.ASCII.GetBytes(s)) |> Array.toList
+sumOfString "foo"
+
+
 module Option = 
 
     // The bind function for Options
@@ -17,3 +31,5 @@ module List =
     // has type : ('a -> 'b list) -> 'a list -> 'b list
 
 // og asynk
+module Async =
+    let bind fn value = async.Bind(value, fn)
