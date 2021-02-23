7b2c3db7b5cff2e74bafb34a8eb7b8acb228ca31
bjartwolf <bjartnes@gmail.com>
2021-02-23 09:49:41 +0100

    fimmy works more

--- ddd-fsharp/Shop/Fimmy.fs
+++ ddd-fsharp/Shop/Fimmy.fs
@@ -8,3 +8,15 @@ let introduceError (percentageFail: int) (fragileFunc : ('a->'b)) : ('a -> 'b) =
             failwith "Network error"
         else fragileFunc x
     crashingFunc
+
+let introduceLatency (chanceOfLatency: int) (latencyInMs: int) (origninalFunc: ('a->'b)) : ('a -> Async<'b>) =
+    let slowFunc x = 
+        let randomNum = randomGenerator.Next(100)
+        async {
+            if randomNum > chanceOfLatency then
+                do! Async.Sleep latencyInMs
+                return origninalFunc x
+            else 
+                return origninalFunc x
+        }
+    slowFunc
